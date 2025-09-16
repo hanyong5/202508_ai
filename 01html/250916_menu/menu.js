f97a35726ca7cd6b33446a869ba7609994273a51
hanyong5 <hanyong5@naver.com>
2025-09-16 11:22:16 +0900

    menu#1

---
+++ 01html/250916_menu/menu.js
@@ -0,0 +1,24 @@
+const menu = [
+  {
+    //menu[0].menu
+    menu: 'company',
+    url: '#',
+  },
+  {
+    //menu[1].menu
+    menu: 'product',
+    url: '#',
+  },
+  {
+    menu: 'oline',
+    url: '#',
+  },
+  {
+    menu: 'community',
+    url: '#',
+  },
+  {
+    menu: 'board',
+    url: '#',
+  },
+];
