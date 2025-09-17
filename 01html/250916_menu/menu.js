52014b92463657ee1a5c68157b31566b11de1008
hanyong5 <hanyong5@naver.com>
2025-09-17 11:27:03 +0900

    script menu

--- 01html/250916_menu/menu.js
+++ 01html/250916_menu/menu.js
@@ -2,23 +2,42 @@ const menu = [
   {
     //menu[0].menu
     menu: 'company',
-    url: '#',
+    url: 'http://www.naver.com',
+    sub: false,
+    submenu: [],
   },
   {
     //menu[1].menu
     menu: 'product',
-    url: '#',
+    url: 'http://www.daum.net',
+    sub: true,
+    submenu: [
+      {
+        menu: 'p01_01',
+        url: '#',
+      },
+      {
+        menu: 'p01_02',
+        url: '#',
+      },
+    ],
   },
   {
     menu: 'oline',
     url: '#',
+    sub: false,
+    submenu: [],
   },
   {
     menu: 'community',
     url: '#',
+    sub: false,
+    submenu: [],
   },
   {
     menu: 'board',
     url: '#',
+    sub: false,
+    submenu: [],
   },
 ];
