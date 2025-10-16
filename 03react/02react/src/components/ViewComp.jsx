64c40efbb5be7deb1858a56fe5e691375b574a7e
hanyong5 <hanyong5@naver.com>
2025-10-16 13:06:35 +0900

    react

--- 03react/02react/src/components/ViewComp.jsx
+++ 03react/02react/src/components/ViewComp.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import Button from './Button';
 
-function ViewComp() {
+function ViewComp(props) {
   return (
     <div>
       <h3>gallery</h3>
+      {/* <p>{props.cityData[0]}</p> */}
+      <div style={{ display: 'flex', gap: '16px' }}>
+        {props.cityData.map((item, i) => {
+          return (
+            <div
+              key={i}
+              style={{
+                background: 'gray',
+                borderRadius: '10px',
+                padding: '10px',
+              }}
+            >
+              <h2>{item}</h2>
+              <Button title="글보기" color="pink"></Button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
