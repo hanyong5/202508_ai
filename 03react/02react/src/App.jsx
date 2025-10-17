8131cbe3ac94f63531dcf55129dba1bfaab93903
hanyong5 <hanyong5@naver.com>
2025-10-17 11:32:14 +0900

    state

--- 03react/02react/src/App.jsx
+++ 03react/02react/src/App.jsx
@@ -1,14 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from './components/state/Button';
 
 function App() {
-  return (
-    <div>
-      <h3>state</h3>
+  const test = 3;
+  const [count, setCount] = useState(1);
+  // const [allCity] = useState([{ city: '서울', cont: 'test' }]);
+  // console.log(count[0]);
+  // console.log(allCity[0]['city']);
 
-      <Button title="글쓰기" color="red" width="200"></Button>
-      <Button></Button>
-    </div>
+  function han() {
+    setCount(count + 1);
+  }
+
+  const han1 = function () {
+    setCount(count + 1);
+  };
+
+  const han2 = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <>
+      <div>
+        <h3>state</h3>
+        <button onClick={han2}>{count}</button>
+        {/* <button onClick={()=>{setCount(count+1)}}>{count}</button> */}
+        <button
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          {count}
+        </button>
+
+        <hr />
+        {test}
+        <Button title="글쓰기"></Button>
+        <Button title="글수정" color="red"></Button>
+        <Button title="글리스트" color="blue"></Button>
+      </div>
+    </>
   );
 }
 
--- 03react/02react/src/components/state/Button.jsx
+++ 03react/02react/src/components/state/Button.jsx
@@ -4,11 +4,19 @@ function Button({ title = '글작성', color = 'skyblue', width = '100' }) {
   //   console.log(props);
   console.log(title, color, width);
   const btnStyle = {
-    width: `${width}px`,
+    display: 'inline-block',
     background: color,
+    borderRadius: '20px',
+    padding: '8px 30px',
+    color: 'white',
+    cursor: 'pointer',
   };
 
-  return <div style={btnStyle}>{title}</div>;
+  return (
+    <>
+      <div style={btnStyle}>{title}</div>
+    </>
+  );
 }
 
 export default Button;
