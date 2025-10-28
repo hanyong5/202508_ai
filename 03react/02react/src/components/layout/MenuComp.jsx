c129bdf659126370c12ea9d1da294cef902fe13b
hanyong5 <hanyong5@naver.com>
2025-10-28 12:00:17 +0900

    router

--- 03react/02react/src/components/layout/MenuComp.jsx
+++ 03react/02react/src/components/layout/MenuComp.jsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 
 function MenuComp() {
   return (
-    <nav class="navbar navbar-expand-lg bg-primary navbar-dark">
-      <div class="container">
-        <Link class="navbar-brand" to="/">
+    <nav className="navbar navbar-expand-lg bg-primary navbar-dark">
+      <div className="container">
+        <Link className="navbar-brand" to="/">
           LOGO
         </Link>
         <button
-          class="navbar-toggler"
+          className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarSupportedContent"
@@ -17,22 +17,22 @@ function MenuComp() {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span class="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <Link class="nav-link active" aria-current="page" to="/">
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/">
                 Home
               </Link>
             </li>
-            <li class="nav-item">
-              <Link class="nav-link active" aria-current="page" to="/about">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/about">
                 About
               </Link>
             </li>
-            <li class="nav-item">
-              <Link class="nav-link active" aria-current="page" to="/board">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/board">
                 Board
               </Link>
             </li>
---
+++ 03react/02react/src/components/pages/home/HomeComp.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function HomeComp() {
+  return <div className="container">HomeComp</div>;
+}
+
+export default HomeComp;
